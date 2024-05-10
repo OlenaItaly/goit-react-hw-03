6375dbb787b22805f258966cab4eb3b1757defc2
Olena Kardash <olena.kardash75@gmail.com>
2024-05-10 11:19:30 +0200

    format2

---
+++ src/componens/App/App.jsx
@@ -0,0 +1,12 @@
+import { ContactForm } from "../ContactForm/ContactForm"
+
+exports default function App() {
+    return (
+      <div>
+  <h1>Phonebook</h1>
+  <ContactForm />
+  <SearchBox />
+  <ContactList />
+</div> 
+   ) 
+}
